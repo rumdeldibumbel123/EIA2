6a2fbf442e0066016f492c12b0726903e16c42c2
danie <danie@LAPTOP-SQ8JMNCJ.eduroam.hs-furtwangen.de>
2017-11-02 20:16:17 +0100

    fg

--- aufgabe_1/skipiste.js
+++ aufgabe_1/skipiste.js
@@ -7,26 +7,26 @@
 window.addEventListener("load", ski);
 function ski() {
     (document.getElementsByTagName("canvas")[0]);
-    var canvas = document.getElementsByTagName("canvas")[0];
+    var canvas = document.getElementsByTagName("canvas")[0]; //
     var crc2 = canvas.getContext("2d");
-    //Himmel
+    // Himmel
     crc2.fillStyle = "#004ae0";
-    crc2.fillRect(0, 0, 800, 600);
-    //Berg2 (Hintergrund)
+    crc2.fillRect(0, 0, 800, 600); // Gr��e des Himmels
+    // Berg 2 (Hintergrund)
     crc2.fillStyle = "#666666";
     crc2.beginPath();
-    crc2.moveTo(250, 100); //h�he
+    crc2.moveTo(250, 100); // H�he
     crc2.lineTo(425, 450);
     crc2.lineTo(75, 450);
     crc2.fill();
-    //Berg3 (Hintergrund)
+    // Berg 3 (Hintergrund)
     crc2.fillStyle = "#434343";
     crc2.beginPath();
-    crc2.moveTo(450, 10); //h�he
+    crc2.moveTo(450, 10); // H�he
     crc2.lineTo(625, 450);
     crc2.lineTo(275, 450);
     crc2.fill();
-    //Wolke1  
+    // Wolke 1  
     crc2.fillStyle = "#ffffff";
     crc2.beginPath();
     crc2.arc(700, 195, 30, 0, 2 * Math.PI);
@@ -37,7 +37,7 @@ function ski() {
     crc2.beginPath();
     crc2.arc(760, 195, 30, 0, 2 * Math.PI);
     crc2.fill();
-    //Wolke2 
+    // Wolke 2 
     crc2.fillStyle = "#ffffff";
     crc2.beginPath();
     crc2.arc(600, 100, 30, 0, 2 * Math.PI);
@@ -48,7 +48,7 @@ function ski() {
     crc2.beginPath();
     crc2.arc(660, 100, 30, 0, 2 * Math.PI);
     crc2.fill();
-    //Wolke3 
+    // Wolke 3 
     crc2.fillStyle = "#ffffff";
     crc2.beginPath();
     crc2.arc(500, 170, 30, 0, 2 * Math.PI);
@@ -59,8 +59,8 @@ function ski() {
     crc2.beginPath();
     crc2.arc(560, 170, 30, 0, 2 * Math.PI);
     crc2.fill();
-    //Skilift
-    crc2.fillStyle = "424242"; //Seilbahn
+    // Skilift
+    crc2.fillStyle = "424242"; // Seilbahn
     crc2.beginPath();
     crc2.moveTo(0, 100);
     crc2.lineTo(800, 370);
@@ -70,14 +70,14 @@ function ski() {
     crc2.lineTo(90, 150);
     crc2.stroke();
     crc2.fillStyle = "#303030";
-    crc2.beginPath(); //Gondel
-    crc2.moveTo(50, 135);
-    crc2.lineTo(130, 160); //Dach der Gondel
-    crc2.lineTo(130, 200); //Rechte Wand
-    crc2.lineTo(50, 175); //Boden
-    crc2.lineTo(50, 145); //Linke Wand
+    crc2.beginPath(); // Gondel
+    crc2.moveTo(50, 135); // Startpunkt Gondel
+    crc2.lineTo(130, 160); // Dach der Gondel
+    crc2.lineTo(130, 200); // Rechte Wand
+    crc2.lineTo(50, 175); // Boden
+    crc2.lineTo(50, 145); // Linke Wand
     crc2.fill();
-    //Baum 1
+    // Baum 1
     crc2.fillStyle = "#342c22";
     crc2.fillRect(550, 275, 20, 150);
     crc2.fillStyle = "#005518";
@@ -96,10 +96,10 @@ function ski() {
     crc2.lineTo(590, 300);
     crc2.lineTo(560, 250);
     crc2.fill();
-    //Boden  
+    // Boden  
     crc2.fillStyle = "#f2f2f2";
     crc2.fillRect(0, 500, 800, 200);
-    //Berg
+    // Berg
     crc2.fillStyle = "#8e8e8e";
     crc2.beginPath();
     crc2.moveTo(0, 230);
