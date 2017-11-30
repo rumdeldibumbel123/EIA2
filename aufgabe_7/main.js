d6fc31ef3894f901a24ddc3bc60656c1a6146cbd
danie <danie@169.254.134.135>
2017-11-30 21:06:16 +0100

    sddf

---
+++ aufgabe_7/main.js
@@ -0,0 +1,64 @@
+/*
+Aufgabe 7: Skipiste mit Interfaces
+Name: Daniel Sand
+Matrikel: 256649
+Datum: 30.11.2017
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+var StudiVZ;
+(function (StudiVZ) {
+    var students = [];
+    var stop = false;
+    while (!stop) {
+        var action = prompt("Was willst du tun, Alter? Zum Datensatz anlegen knall N. Zum Homie abchecken A. Bock auf nix? Knall X");
+        switch (action) {
+            case "n":
+            case "N":
+                var input = prompt("Gib mir dein Essensgeld oder deine Daten (jeweils mit Komma getrennt)! von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 -> w oder 1 -> m) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel = parseInt(prompt("Martikelnummer her, sonst..."));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+    function saveData(_input) {
+        var dataArray = _input.split(","); //split gibt ein Array zur�ck
+        var studentData = {
+            matrikelnummer: parseInt(dataArray[0]),
+            name: dataArray[1],
+            vorname: dataArray[2],
+            alter: parseInt(dataArray[3]),
+            geschlecht: parseInt(dataArray[4]) == 0,
+            kommentar: dataArray[5]
+        };
+        students.push(studentData);
+        var gender;
+        if (parseInt(dataArray[4]) == 0) {
+            gender = "weiblich";
+        }
+        else {
+            gender = "m�nnlich";
+        }
+        return "Hier hast dein Zeug. Und jetzt verschwinde!\n" + "\nMatrikelnummer: " + studentData.matrikelnummer + "\nName: " + studentData.vorname + studentData.name + "\nAlter: " + studentData.alter + "\nGeschlecht: " + +studentData.geschlecht + "\nKommentar: " + studentData.kommentar;
+    }
+    function queryData(_matrikel) {
+        for (var i = 0; i < students.length; i++) {
+            if (students[i].matrikelnummer == _matrikel) {
+                var geschlecht = students[i].geschlecht ? "weiblich" : "m�nnlich";
+                return "Da hast du: " + students[i].matrikelnummer + "\n\nName: " + students[i].vorname + students[i].name + "\nAlter: " + students[i].alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + students[i].kommentar;
+            }
+            else {
+                return "Die Nummer gibts doch garnicht. Willst du mich verarschen?";
+            }
+        }
+    }
+})(StudiVZ || (StudiVZ = {}));
---
+++ aufgabe_7/main.ts
@@ -0,0 +1,82 @@
+/*
+Aufgabe 7: Skipiste mit Interfaces
+Name: Daniel Sand
+Matrikel: 256649
+Datum: 30.11.2017
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. 
+*/
+
+namespace StudiVZ {
+    interface StudentData {
+        // Datenstruktur
+        matrikelnummer: number;
+        name: string;
+        vorname: string;
+        alter: number;
+        geschlecht: boolean;
+        kommentar: string;
+    }
+
+    var students: StudentData[] = [];
+    var stop: boolean = false;
+
+    while (!stop) {
+        var action: string = prompt("Was willst du tun, Alter? Zum Datensatz anlegen knall N. Zum Homie abchecken A. Bock auf nix? Knall X");
+
+        switch (action) {
+            case "n":
+            case "N":
+                var input: string = prompt("Gib mir dein Essensgeld oder deine Daten (jeweils mit Komma getrennt)! von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 -> w oder 1 -> m) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel: number = parseInt(prompt("Martikelnummer her, sonst..."));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+
+    function saveData(_input: string): string {
+        let dataArray: string[] = _input.split(",");   //split gibt ein Array zurück
+        let studentData: StudentData = {
+            matrikelnummer: parseInt(dataArray[0]),          //parseInt wandelt einen String in eine Number um
+            name: dataArray[1],                    //nimmt mit [1] das zweite Element aus dem Array
+            vorname: dataArray[2],
+            alter: parseInt(dataArray[3]),
+            geschlecht: parseInt(dataArray[4]) == 0,
+            kommentar: dataArray[5]
+        };
+
+        students.push(studentData);
+
+        let gender: string;
+        if (parseInt(dataArray[4]) == 0) {
+            gender = "weiblich";
+        }
+        else {
+            gender = "männlich";
+        }
+
+        return "Hier hast dein Zeug. Und jetzt verschwinde!\n" + "\nMatrikelnummer: " + studentData.matrikelnummer + "\nName: " + studentData.vorname + studentData.name + "\nAlter: " + studentData.alter + "\nGeschlecht: " + + studentData.geschlecht + "\nKommentar: " + studentData.kommentar;
+    }
+
+    function queryData(_matrikel: number): string {
+        for (let i: number = 0; i < students.length; i++) {
+            if (students[i].matrikelnummer == _matrikel) {
+                let geschlecht: string = students[i].geschlecht ? "weiblich" : "männlich";
+                return "Da hast du: " + students[i].matrikelnummer + "\n\nName: " + students[i].vorname + students[i].name + "\nAlter: " + students[i].alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + students[i].kommentar;
+            }
+
+            else {
+                return "Die Nummer gibts doch garnicht. Willst du mich verarschen?";
+            }
+        }
+    }
+}
